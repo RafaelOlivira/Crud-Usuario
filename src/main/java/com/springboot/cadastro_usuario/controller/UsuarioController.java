54158ae0de3a11d3ba4e491eab6221d0c42e14b4
Rafael <rafaeloliveirasouzasil@gmail.com>
2026-01-06 16:11:18 -0300

    Controller do usuário criado com sucesso!

--- src/main/java/com/springboot/cadastro_usuario/controller/UsuarioController.java
+++ src/main/java/com/springboot/cadastro_usuario/controller/UsuarioController.java
@@ -1,4 +1,38 @@
 package com.springboot.cadastro_usuario.controller;
 
+import com.springboot.cadastro_usuario.business.UsuarioService;
+import com.springboot.cadastro_usuario.infrastructure.entitys.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email){
+        return ResponseEntity.ok(usuarioService.buscarPorEmail(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        usuarioService.deletarUsuarioEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Long id, @RequestBody Usuario usuario){
+        usuarioService.atualizarUsuarioPorId(id,usuario);
+        return ResponseEntity.ok().build();
+    }
 }
